27d399a1df1305f49025d81970061be6d2584a1b
shahadAlsayed <60428361+shahadAlsayed@users.noreply.github.com>
2023-01-04 13:32:11 +0300

    contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,41 +10,59 @@ function contact(){
     <section id="contactSection">
     <Typography className='title' variant='h3' sx={{color:'#3F3F3F' , fontWeight:'bold' , justifyContent:'center', display:'flex', paddingTop:'4rem' , paddingBottom:'2rem',marginTop:'4em'}}>Contact</Typography>
     <Box sx={{width:'60%',display:'flex',justifyContent:'center',marginLeft:'auto',marginRight:'auto',marginBottom:'8em'}}>
-      <Box  className='pattren' sx={{bgcolor:"#F2E2E3"}}>
+
+      <Box  className='pattren' sx={{bgcolor:"#F2E2E3",borderRadius:'5px'}}>
       <Box className='test' sx={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',width:'80% '}}>
             <Typography className='scan'  variant='h6' sx={{color:'#3F3F3F' , fontWeight:'bold' }}> Scan the QR</Typography>
             <img className='qr' src={QR}></img>
         </Box>
       </Box>
       
-      <Box sx={{width:'80%',justifyContent:'center',display:'flex',flexDirection:'column',border:'10px solid #F2E2E3'}}>
+      <Box sx={{width:'80%',justifyContent:'center',display:'flex',flexDirection:'column'}}>
+
+        {/* //linkedin */}
+        <a  className='card' href='https://www.linkedin.com/in/shahad-alsayed-662792203' >
         <Box sx={{display:'flex', flexDirection:'row', paddingLeft:'2em',alignItems:'center'}}>
-        <a className='linkedinIcon icon' href='https://www.linkedin.com/in/shahad-alsayed-662792203'>
+        {/* <a className='linkedinIcon icon' href='https://www.linkedin.com/in/shahad-alsayed-662792203'> */}
         <img className='linkedinIcon icon' ></img>
-        </a>
+        {/* </a> */}
           <Box  sx={{display:'flex', flexDirection:'column',paddingTop:'2rem' , paddingBottom:'2rem',paddingLeft:'1em'}}>
           <Typography variant='h8' sx={{color:'#3F3F3F' , fontWeight:'bold' , display:'flex' }}>Linkedin</Typography>
-          <Typography variant='h8' sx={{color:'#3F3F3F' ,  display:'flex',paddingTop:'5px'}}>Shahad Alsayed</Typography>
+
+          <Typography variant='h8' sx={{color:'#3F3F3F' ,  display:'flex',paddingTop:'5px'}}>  Shahad Alsayed  </Typography>
           </Box>
         </Box>  
+        </a>
+
+        {/* phone */}
+        <a  className='card' href='tel:+966549554621' >
         <Box sx={{display:'flex', flexDirection:'row',alignItems:'center', paddingLeft:'2em'}}>
           <a className='phoneIcon icon'href='tel:+966549554621'>
           <img ></img>
           </a>
           <Box sx={{display:'flex', flexDirection:'column',paddingTop:'2rem' , paddingBottom:'2rem',paddingLeft:'1em'}}>
           <Typography variant='h8' sx={{color:'#3F3F3F' , fontWeight:'bold' , display:'flex' }}>Phone</Typography>
-          <Typography variant='h8' sx={{color:'#3F3F3F' ,  display:'flex',paddingTop:'5px'}}>+966 549554621</Typography>
+          <Typography variant='h8' sx={{color:'#3F3F3F' ,  display:'flex',paddingTop:'5px'}}> +966 549554621</Typography>
           </Box>
         </Box>
+        </a>
+
+
+        {/* email */}
+        <a  className='card' href='mailto:ShahadAlsayed44@gmail.com'>
         <Box sx={{display:'flex', flexDirection:'row',alignItems:'center', paddingLeft:'2em'}}>
           <a className='emailIcon icon' href='mailto:ShahadAlsayed44@gmail.com'>
           <img  ></img>
           </a>
+
           <Box className='emailClass' sx={{display:'flex', flexDirection:'column',paddingTop:'2rem' , paddingBottom:'2rem',paddingLeft:'1em'}}>
           <Typography variant='h8' sx={{color:'#3F3F3F' , fontWeight:'bold' , display:'flex' }}>Email</Typography>
           <Typography variant='h8' sx={{color:'#3F3F3F' ,  display:'flex',paddingTop:'5px'}}>ShahadAlsayed44@gmail.com</Typography>
           </Box>
+
         </Box>
+        </a>
+        
       </Box>
     </Box>
 
